ba99457d89e4c4f59d41710d6b9d12038abeeb8d
Egor Volkov <eivolkov@edu.hse.ru>
2026-01-22 16:11:21 +0300

    Fixed bugs in submodule

--- source/app.cpp
+++ source/app.cpp
@@ -2,17 +2,17 @@
 #include "app.h"
 
 int main(){
-    try {
-        auto[filemane, key_from, key_to] = parse_arguments(arg_count, arg_vars);
-        matrix_t matr = load_matrix(filename);
-        validate(matr, key_from, key_to);
-        auto [weight, route] = compute_route(matr, key_from, key_to);
-        components_t components = compute_components(matr);
-        print_shortest_route(weight, route);
-        print_components(components);
-    }
-    catch(const std::exception& except){
-        sdt::cerr <<"ERROR:" << e.what() << '\n';
-    }
-    
+    // try {
+    //     auto[filemane, key_from, key_to] = parse_arguments(arg_count, arg_vars);
+    //     matrix_t matr = load_matrix(filename);
+    //     validate(matr, key_from, key_to);
+    //     auto [weight, route] = compute_route(matr, key_from, key_to);
+    //     components_t components = compute_components(matr);
+    //     print_shortest_route(weight, route);
+    //     print_components(components);
+    // }
+    // catch(const std::exception& except){
+    //     sdt::cerr <<"ERROR:" << e.what() << '\n';
+    // }
+    return 0;
 }
