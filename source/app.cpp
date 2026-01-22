76ff84600d9ce0f0d02d385dadd7a0a9e754512d
Egor Volkov <eivolkov@edu.hse.ru>
2026-01-22 16:24:51 +0300

    Added argument-parsing function

--- source/app.cpp
+++ source/app.cpp
@@ -16,3 +16,78 @@ int main(){
     // }
     return 0;
 }
+
+
+std::tuple<std::string, node_name_t, node_name_t> parse_args(int argc, char* argv[]){
+    if (argc < 7)
+        throw std::runtime_error("Invalid number of arguments\nUsage: graph --file <path-name> --from <node-index> --to <node-index>");
+
+    std::string filename;
+    bool has_file = false;
+    bool has_from = false;
+    bool has_to   = false;
+    node_name_t from = 0;
+    node_name_t to   = 0;
+    std::unordered_set<std::string> seen;
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+        if (arg == "--file") {
+
+            if (!seen.insert(arg).second)
+                throw std::runtime_error("Duplicated argument: --file");
+
+            if (i + 1 >= argc)
+                throw std::runtime_error("Missing value for --file");
+
+            filename = argv[++i];
+            has_file = true;
+        }
+
+        else if (arg == "--from") {
+            
+            if (!seen.insert(arg).second)
+                throw std::runtime_error("Duplicated argument: --from");
+
+            if (i + 1 >= argc)
+                throw std::runtime_error("Missing value for --from");
+
+            char* end = nullptr;
+            unsigned long v = std::strtoul(argv[++i], &end, 10);
+            if (*end != '\0')
+                throw std::runtime_error("Invalid node: " + std::string(argv[i]));
+
+            from = static_cast<node_name_t>(v);
+            has_from = true;
+        }
+
+        else if (arg == "--to") {
+
+            if (!seen.insert(arg).second)
+                throw std::runtime_error("Duplicated argument: --to");
+
+            if (i + 1 >= argc)
+                throw std::runtime_error("Missing value for --to");
+
+            char* end = nullptr;
+            unsigned long v = std::strtoul(argv[++i], &end, 10);
+            if (*end != '\0')
+                throw std::runtime_error("Invalid node: " + std::string(argv[i]));
+
+            to = static_cast<node_name_t>(v);
+            has_to = true;
+        }
+
+        else 
+            throw std::runtime_error("Invalid argument: " + arg + "\nUsage: graph --file <path-name> --from <node-index> --to <node-index>");
+            
+    }
+
+    if (!has_file || !has_from || !has_to) {
+        throw std::runtime_error(
+            "Missing required arguments\n"
+            "Usage: graph --file <path-name> --from <node-index> --to <node-index>"
+        );
+    }
+
+    return { filename, from, to };
+}
