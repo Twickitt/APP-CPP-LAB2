d2e3c22cfb9806892f33f8b73496758dc821c94c
Egor Volkov <eivolkov@edu.hse.ru>
2026-01-22 15:59:38 +0300

    Alias and main structure were added

--- source/app.cpp
+++ source/app.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
- 
+#include "app.h"
+
 int main(){
-    std::cout<<"Hi";
-    return 0;
+    try {
+        auto[filemane, key_from, key_to] = parse_arguments(arg_count, arg_vars);
+        matrix_t matr = load_matrix(filename);
+        validate(matr, key_from, key_to);
+        auto [weight, route] = compute_route(matr, key_from, key_to);
+        components_t components = compute_components(matr);
+        print_shortest_route(weight, route);
+        print_components(components);
+    }
+    catch(const std::exception& except){
+        sdt::cerr <<"ERROR:" << e.what() << '\n';
+    }
+    
 }
